8d0d5a6f5b13d3d58ee1f06ca32a195f8c68cc1f
aKrRaC <akhilrac2000@gmail.com>
2021-01-05 22:52:22 +0530

    minor fix

--- lib/pages/Search.dart
+++ lib/pages/Search.dart
@@ -28,8 +28,8 @@ class Search extends StatelessWidget {
           child: Stack(
             children: <Widget>[
               Container(
-                width: 156,
-                height: 92,
+                width: 165,
+                height: 100,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(10)),
                     color: Colors.blue[800],
@@ -103,7 +103,7 @@ class Search extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(12, 20, 12, 15),
+              padding: EdgeInsets.fromLTRB(6, 20, 6, 15),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
